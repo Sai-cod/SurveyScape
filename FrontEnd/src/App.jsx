b23c4e00651afd74eec7b68101e902c67fcf3c15
Sairaj <ekbotesairaj640@gmail.com>
2025-01-29 11:18:46 +0530

    context added

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Adlogin from './components/Admin/Adlogin'
 import Landing from './components/Landing'
-import Adsign from './components/Adsign'
+import Adsign from './components/Admin/Adsign'
 function App() {
   return (
     <div>
--- FrontEnd/src/components/Admin/Adsign.jsx
+++ FrontEnd/src/components/Admin/Adsign.jsx
@@ -1,22 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Adsign() {
+  const [email, setemail] = useState("");
+  const [username, setusername] = useState("");
+  const [pass, setpass] = useState("");
+  const object = {username, email, pass};
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    
+  }
   return (
     <div id="sign-back" className="w-full h-screen bg-gradient-to-r from-[#141e30] to-[#243b55] flex justify-center items-center">
       <div className="sigcard w-[90%] md:w-[400px] bg-[#1c1c1c88] backdrop-blur-lg rounded-[20px] p-8">
         <h1 className="text-4xl text-white text-center">Sign Up</h1>
-        <form className="w-full mt-8 flex flex-col space-y-6">
-          <input
+        <form onSubmit={(e)=>{
+          handleSubmit(e);
+        }} 
+        className="w-full mt-8 flex flex-col space-y-6">
+          <input onChange={(e)=>{
+            setusername(e.target.value);
+          }}
             className="w-full h-[50px] rounded-full placeholder:text-white bg-[#ffffff22] text-white px-5 text-lg outline-none focus:ring-2 focus:ring-[#3ac47d]"
             type="text"
             placeholder="UserName"
           />
           <input
+            onChange={(e)=>{
+              setemail(e.target.value);
+            }}
             className="w-full h-[50px] rounded-full placeholder:text-white bg-[#ffffff22] text-white px-5 text-lg outline-none focus:ring-2 focus:ring-[#3ac47d]"
             type="email"
             placeholder="Email"
           />
           <input
+             onChange={(e)=>{
+              setpass(e.target.value);
+            }}
             className="w-full h-[50px] rounded-full placeholder:text-white bg-[#ffffff22] text-white px-5 text-lg outline-none focus:ring-2 focus:ring-[#3ac47d]"
             type="password"
             placeholder="Password"
